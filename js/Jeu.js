3900e0c7cac139f2141f973fdc7adb72d2a9801f
Quentin <21802953@etu.unicaen.fr>
2019-03-26 21:40:37 +0100

    Changement de message de fin pour afficher le tour.

--- js/Jeu.js
+++ js/Jeu.js
@@ -111,7 +111,7 @@ class Jeu {
                 if (Jeu.joueurs[1].getEstUneIa())
                 document.getElementById("transition-tips").innerHTML = "Bateau trouvé en " +Jeu.numTour+ " tours, bien joué !";
                 else
-                    document.getElementById("transition-tips").innerHTML = Jeu.joueurs[Plateau.i == 1 ? 0 : 1].getNom()+ " tire comme un pied !";
+                    document.getElementById("transition-tips").innerHTML = Jeu.joueurs[Plateau.i == 1 ? 0 : 1].getNom()+ " a coulé ! (" +Jeu.numTour+ " tours)";
                 document.getElementById("bouton-pret").innerHTML = "REVENIR AU MENU";
                 if (Jeu.numTour != -1) document.body.style.backgroundImage = "url(images/fond_victoire.gif), url(images/fond_victoire.jpg)";
                     Jeu.numTour = -1;
