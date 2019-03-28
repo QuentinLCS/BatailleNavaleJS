099c43a04f8a630d0ef8c1cfa6b709e564ffb4cf
Quentin <21802953@etu.unicaen.fr>
2019-03-28 17:23:41 +0100

    Fix affichage du bateau lorsqu'il n'est pas init

--- js/Jeu.js
+++ js/Jeu.js
@@ -95,13 +95,14 @@ class Jeu {
                 Affichage.playSound(0);
                 Jeu.plateau.tableau[coordonnees.getX()][coordonnees.getY()].setProche(Plateau.i, true);
                 if (Jeu.plateau.getTaille() == 20) {
-                    Jeu.plateau.tableau[Jeu.joueurs[adversaire].bateaux[Plateau.j].getPoints()[0].getX()][Jeu.joueurs[adversaire].bateaux[Plateau.j].getPoints()[0].getY()].setBateau(adversaire, false);
+                    Jeu.plateau.tableau[Jeu.joueurs[adversaire].bateaux[Plateau.j].getPoints()[0].getX()][Jeu.joueurs[adversaire].bateaux[Plateau.j].getPoints()[0].getY()].setBateau(adversaire, false);                  
                     setTimeout(function() {
                         if (Jeu.joueurs[1].getEstUneIa()) {
                             Jeu.joueurs[adversaire].bateaux[Plateau.j].getPoints()[0].setX(Jeu.getRandomInt(Jeu.plateau.getTaille()));
                             Jeu.joueurs[adversaire].bateaux[Plateau.j].getPoints()[0].setY(Jeu.getRandomInt(Jeu.plateau.getTaille()));
-                        } else
+                        } else {
                             Jeu.joueurs[adversaire].setEstInit(false);
+                        }
                         Jeu.plateau.clearInts(Plateau.i);
                     }, Jeu.joueurs[1].getEstUneIa() ? 1000 : 2000);
                 }
@@ -113,10 +114,10 @@ class Jeu {
     static transition(forceHome) {
         Affichage.regles(true);
         if (!Jeu.plateau.getClickIsOn() && !forceHome) {
-            if (Jeu.joueurs[0].getEstInit() && Jeu.joueurs[1].getEstInit() && Jeu.numTour == 1) {
+            if (Jeu.joueurs[0].getEstInit() && Jeu.joueurs[1].getEstInit() && Jeu.numTour == 1)
                 document.body.style.backgroundImage = "none, url(images/fond_jeu.jpg)";
-                Affichage.bateau(true);
-            } if (!Jeu.joueurs[Plateau.i].getNbBateauxVivants()) {
+                
+            if (!Jeu.joueurs[Plateau.i].getNbBateauxVivants()) {
                 Affichage.playSound(2);
                 document.getElementById("transition-texte").innerHTML = "Victoire de";
                 if (Jeu.joueurs[1].getEstUneIa())
@@ -163,6 +164,11 @@ class Jeu {
                 document.getElementById("partie").style.display = "flex";
                 Jeu.plateau.afficherPlateau();
                 Affichage.infosTour();
+                if (Jeu.numTour) {
+                    Affichage.bateau(true);
+                    if (!Jeu.joueurs[0].getEstInit() || !Jeu.joueurs[1].getEstInit())
+                        Affichage.bateau();  
+                }
                 Jeu.numTour += Plateau.i;
                 if (!Jeu.joueurs[0].getEstInit() || !Jeu.joueurs[1].getEstInit())
                     Affichage.changerCurseurs("cursor-addBoat");                        
